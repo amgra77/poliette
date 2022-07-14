0cccba53ada403b40d62fd8704bc29ef6e3b0949
Alejandro Moreno <carlos_y_laura@hotmail.com>
2022-07-14 08:25:33 -0400

    First draft to homepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#3F51B5'
+      }
+    },
   },
   plugins: [],
 }
