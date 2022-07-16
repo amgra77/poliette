2afd0f51ff2e8ed735f6e2248003a5ee6323d90b
Alejandro Moreno <carlos_y_laura@hotmail.com>
2022-07-16 06:13:20 -0400

    Wrong import library

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-import Link from 'react/link';
+import Link from 'next/link';
 
 const Home: NextPage = () => {
     const router = useRouter();
